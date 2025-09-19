a695edae28f3c4a99850c4f66fea406ad4e81dcf
martineesmae <martineesma@gmail.com>
2025-09-19 10:39:36 +0300

    Algne versioon HKHK_Skriptimine projektist

---
+++ suvaline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#See on minu esimene bash'i skript
+
+TERVITUS="See on tervitus AI poolt"
+
+
+echo $TERVITUS
+echo "Sina oled $(whoami), mina olen AIn XD"
+echo "Tore oli sinuga tuttavaks saada!"
+
